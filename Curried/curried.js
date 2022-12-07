6d152067bc8b32ce897c6540014fa68bb3fbc116
Hala Abuzant <halamabuzant@gmail.com>
2022-12-07 13:51:26 +0200

    firstreact

---
+++ Curried/curried.js
@@ -0,0 +1,8 @@
+function ninjaBelt(ninja){
+    return function belt(beltColor){ //note the closure here!
+      console.log("Ninja "+ ninja + " has earned a " + beltColor +" belt.");
+    }
+  }
+  ninjaBelt('Eileen')('black'); //note the double invocation here.
+  
+  
